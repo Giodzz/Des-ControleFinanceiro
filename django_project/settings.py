321f3d0100a4ae6137221ddf0607673113c95f3f
Hugo Alex <huggoalexoliveira@gmail.com>
2024-04-18 23:19:29 +0000

    Parâmetros do banco de dados adicionados no respectivo arquivo.

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dcfdatabase',
+        'USER': 'dcfuser',
+        'PASSWORD': 'dcfkey',
+        'HOST': 'hasmainserver.duckdns.org',
+        'PORT': '',
     }
 }
 
