779e393a4728f1b4bf69fecdbaf129a32e6349c2
Giodzz <giordanabbucci@gmail.com>
2024-07-23 22:48:49 -0300

    tabela de eventos mostrando no index

--- django_project/descontrole/views/evento.py
+++ django_project/descontrole/views/evento.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from ..forms.evento import EventoForm
+from ..models.evento import Evento
 
 def evento_index(request):
     form = EventoForm()
-    return render(request, "descontrole/evento/index.html", {"form": form})
+    eventos = Evento.objects.all()
+    return render(request, "descontrole/evento/index.html", {"form": form, "eventos": eventos})
